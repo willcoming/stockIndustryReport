8a2317ec4e188d372d05cc9c4283be969236fbe7
willcoming <>
2026-07-28 23:07:48 +0800

    Publish Stock Industry Deep Research site

--- data/stocks.js
+++ data/stocks.js
@@ -1,5 +1,5 @@
 window.STOCK_INDUSTRY_REPORTS = {
-  "generated_at": "2026-07-28T13:35:33.056312+00:00",
+  "generated_at": "2026-07-28T15:07:47.569582+00:00",
   "baseUrl": "https://willcoming.github.io/stockIndustryReport/",
   "base_url": "https://willcoming.github.io/stockIndustryReport/",
   "valid_days": 7,
